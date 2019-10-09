a62c057d74be88c65b93b3c216c67ebc4d788b14
David Hollman <dshollm@sandia.gov>
2019-10-09 15:53:27 -0700

    started sketching plan for reducer CPOs

--- core/src/Kokkos_Reducer.hpp
+++ core/src/Kokkos_Reducer.hpp
@@ -48,9 +48,79 @@
  *
  *  @brief Functionality related to the generic concept of a Kokkos Reducer
  *
+ *  The modern approach to scalable integration of user-customizable concepts
+ *  (like the Kokkos Reducer concept) is to use separable namespace-scope
+ *  customization functions (called "customization point objects," or CPOs)---
+ *  think `std::begin()` instead of a monolithic customizable traits class
+ *  like `std::allocator_traits`.
+ *
+ *  The current set of CPOs and other namespace traits that we need for the
+ *  existing Kokkos Reducer functionality are described below.  We'll put these
+ *  CPOs in the namespace `Kokkos::Experimental::Reducers`.
+ *
+ *  (Note that this is trying to describe the current state of affairs with as
+ *  few changes as possible, and arrays make this kind of a mess.)
+ *
+ *  `Reducers::value_type<R>`
+ *  -------------------------
+ *
+ *  (also includes convenience alias template `value_type_t`)
+ *
+ *  May be specialized. The default implementation contains a  member type named
+ *  `type` that is defined as:
+ *
+ *    * `R::value_type`, if that type name is well-formed,
+ *    * Otherwise, there is not member named `type`.
+ *
+ *  `value_type<R>::type` must be move-constructible.
+ *
+ *  If `typename Reducers::value_type<R>::type` is not well-formed, `R` does not
+ *  meet the requirements of `Reducer`.
+ *
+ *  `Reducers::result_view_type<R>`
+ *  -------------------------------
+ *
+ *  (also includes convenience alias template `result_view_type_t`)
+ *
+ *  May be specialized. The default implementation contains a  member type named
+ *  `type` that is defined as:
+ *
+ *    * `R::result_view_type`, if that type name is well-formed,
+ *    * Otherwise, there is not member named `type`.
+ *
+ *  If `typename Reducers::result_view_type<R>::type` is not well-formed, `R`
+ *  does not meet the requirements of `Reducer`.
+ *
+ *  `auto Reducers::init(R&& r) -> Reducers::value_type_t<R>`
+ *  ---------------------------------------------------------
+ *
+ *  TODO convert this logic to a two-argument version
+ *
+ *    * If the expression `((R&&)r).init()` is well-formed,
+ *      convertible to `value_type_t<R>`, equivalent to `((R&&)r).init()`;
+ *    * otherwise, if the unqualified function call expression `init((R&&)r)` is
+ *      well-formed (in a context that doesn't include `Reducers::init`),
+ *      convertible to `value_type_t<R>`, equivalent to `init((R&&)r)`;
+ *    * otherwise, if `((R&&)r).init(vref)` is well-formed given `vref` of type
+ *      `value_type_t<R>&`, if `value_type_t<R>` is default constructible,
+ *      and if `result_view_type_t<R>::rank == 0`, equvalent to
+ *      `auto v = value_type_t<R>{}; ((R&&)r).init(v); return v;`;
+ *    * otherwise, if `value_type_t<R>` is default constructible and
+ *      `result_view_type_t<R>::rank == 1`,  TODO finish this
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
 namespace Kokkos {
+
+namespace Experimental {
+
+} // end namespace Experimental
 
 } // end namespace Kokkos
 
